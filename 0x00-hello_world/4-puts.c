cb35de8e9cc5ff0d11575c51b8cf035b13cc4f17
iamomari <omarisanya@gmail.com>
2023-06-16 04:15:19 -0700

    added new file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
  /**
- * main : Represents the entry point for code execution
+ * main - Entry point
  *  
  * Return : Always 0 (success)
  */
 int main(void)
 {
-	puts ("\"programming is like building a multilingual puzzle");
-	return(0);
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
 }
+
