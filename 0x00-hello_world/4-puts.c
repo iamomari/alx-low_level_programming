938bcfa719070301ff4ea5caef956c6b2dac7b40
iamomari <omarisanya@gmail.com>
2023-06-15 18:20:43 -0700

    added new files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+/**
+ * main : Represents the entry point for code execution
+ *  
+ * Return : Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"programmimg is like building a multilimgual puzzle);
+	return(0);
+} 
