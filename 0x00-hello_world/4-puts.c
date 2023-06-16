410b46109f77d35594358f39fd9d21c3d086f47e
iamomari <omarisanya@gmail.com>
2023-06-16 04:48:14 -0700

    new bug fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *  
- * Return : Always 0 (success)
- */
+
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return(0);
+    printf("Programming is like building a multilingual puzzle\n");
+    return 0;
 }
