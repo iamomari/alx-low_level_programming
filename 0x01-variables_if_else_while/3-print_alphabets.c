61a9f940cfe70422b2988ea1dec5d54a91cefd02
iamomari <omarisanya@gmail.com>
2023-06-18 07:37:46 -0700

    new files

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * prints lower and upper case alphabets
+ *
+ * Return: Always 0 (That is Success)
+ */
+
+int main(void)
+{
+	char LU_case;
+
+	for (LU_case = 'a'; LU_case <= 'z'; LU_case++)
+		putchar(LU_case);
+	for (LU_case = 'A'; LU_case <= 'Z'; LU_case++)
+		putchar(LU_case);
+	putchar('\n');
+
+	return (0);
+}
