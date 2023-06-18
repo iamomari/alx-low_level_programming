fcaa8ff8c8010a4b629daaa5b7a8f391199a137c
iamomari <omarisanya@gmail.com>
2023-06-18 07:56:07 -0700

    new files

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
  * main - Entry point
- *
- * Return: Always 0 (That is Success)
+ * A program that prints all the possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
+        int n;
 
-	for (n = '0'; n = '9'; n++)
-		putchar(n);
-	{
-	if (n != '9')
-		putchar(',');
-	putchar(' ');
-	}
-	putchar('\n');
+        for (n = '0'; n <= '9'; n++) {
+                putchar(n);
+                if (n != '9') {
+                        putchar(',');
+                        putchar(' ');
+                }
+        }
+        putchar('\n');
 
-	return (0);
+        return (0);
 }
