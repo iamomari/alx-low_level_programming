30527a758ad432c691a5ec05ce70ea0c1063e791
iamomari <omarisanya@gmail.com>
2023-06-18 08:05:55 -0700

    bug fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,20 +3,23 @@
 /**
  * main - Entry point
  * A program that prints all the possible combinations of single-digit numbers
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        for (n = '0'; n <= '9'; n++) {
-                putchar(n);
-                if (n != '9') {
-                        putchar(',');
-                        putchar(' ');
-                }
-        }
-        putchar('\n');
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
