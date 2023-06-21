b1a84a00655a36f7f434e8a3bd32e993a3cbd523
iamomari <omarisanya@gmail.com>
2023-06-20 19:34:37 -0700

    new files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - Entry point 
+ *
+ * short Description: this prints '_putchar', followd by a new line.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char mir[] = "_putchar";
+	int length = strlen(mir), i;
+
+	for (i = 0; i < length; i++)
+		_putchar(mir[i]);
+	-putchar('\n');
+
+	return(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A directory that contains C functions and Nested Loops 
