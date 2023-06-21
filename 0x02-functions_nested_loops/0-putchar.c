297db99c814c5882d99ad193b6a85c36cd562dec
iamomari <omarisanya@gmail.com>
2023-06-21 11:54:12 -0700

    bug fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,18 +4,19 @@
 /**
  * main - Entry point
  *
- * short Description: this prints '_putchar', followd by a new line.
+ * Description: Prints '_putchar', followed by a new line.
  *
  * Return: Always 0
  */
 int main(void)
 {
-	char mir[] = "_putchar";
-	int length = strlen(mir), i;
+        char mir[] = "_putchar";
+        int length = strlen(mir);
+        int i;
 
-	for (i = 0; i < length; i++)
-		_putchar(mir[i]);
-	_putchar('\n');
+        for (i = 0; i < length; i++)
+                _putchar(mir[i]);
+        _putchar('\n');
 
-	return (0);
+        return (0);
 }
