32ec61688c6998122afb9afc05ea710a6f896207
iamomari <omarisanya@gmail.com>
2023-06-27 17:31:38 -0700

    betty fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98.c - updates the value of an int using a ponter
- * @n: pointer to an int
+ * reset_to_98.c - Updates the value of an integer to 98
+ * using a ponter.
+ * @n: pointer to an int.
  */
 void reset_to_98(int *n)
 {
